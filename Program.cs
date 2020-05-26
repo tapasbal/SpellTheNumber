c6c15cea9e9d644363929fa21198b936ed97530f
tapasbal <35982161+tapasbal@users.noreply.github.com>
2020-05-26 22:46:19 +0530

    Spell the number completed

--- Program.cs
+++ Program.cs
@@ -115,10 +115,161 @@ namespace SpellTheNumber
             }
             return name;
         }
+        private static String ConvertWholeNumber(String Number)
+        {
+            string word = "";
+            try
+            {
+                bool beginsZero = false;
+                bool isDone = false;  
+                double dblAmt = (Convert.ToDouble(Number));
+               
+                if (dblAmt > 0)
+                {    
+                    beginsZero = Number.StartsWith("0");
+
+                    int numDigits = Number.Length;
+                    int pos = 0;
+                    String place = "";   
+                    switch (numDigits)
+                    {
+                        case 1:   
+
+                            word = Ones(Number);
+                            isDone = true;
+                            break;
+                        case 2:  
+                            word = Tens(Number);
+                            isDone = true;
+                            break;
+                        case 3:  
+                            pos = (numDigits % 3) + 1;
+                            place = " Hundred ";
+                            break;
+                        case 4:    
+                        case 5:
+                        case 6:
+                            pos = (numDigits % 4) + 1;
+                            place = " Thousand ";
+                            break;
+                        case 7: 
+                        case 8:
+                        case 9:
+                            pos = (numDigits % 7) + 1;
+                            place = " Million ";
+                            break;
+                        case 10:
+                        case 11:
+                        case 12:
+
+                            pos = (numDigits % 10) + 1;
+                            place = " Billion ";
+                            break;
+                           
+                        default:
+                            isDone = true;
+                            break;
+                    }
+                    if (!isDone)
+                    { 
+                        if (Number.Substring(0, pos) != "0" && Number.Substring(pos) != "0")
+                        {
+                            try
+                            {
+                                word = ConvertWholeNumber(Number.Substring(0, pos)) + place + ConvertWholeNumber(Number.Substring(pos));
+                            }
+                            catch { }
+                        }
+                        else
+                        {
+                            word = ConvertWholeNumber(Number.Substring(0, pos)) + ConvertWholeNumber(Number.Substring(pos));
+                        }
+
+                            
+                    }
+                      
+                    if (word.Trim().Equals(place.Trim())) word = "";
+                }
+            }
+            catch { }
+            return word.Trim();
+        }
+
+
+        private static String ConvertToWords(String numb)
+        {
+            String val = "", wholeNo = numb, points = "", andStr = "", pointStr = "";
+            String endStr = "Only";
+            try
+            {
+                int decimalPlace = numb.IndexOf(".");
+                if (decimalPlace > 0)
+                {
+                    wholeNo = numb.Substring(0, decimalPlace);
+                    points = numb.Substring(decimalPlace + 1);
+                    if (Convert.ToInt32(points) > 0)
+                    {
+                        andStr = "and";    
+                        endStr = "Paisa " + endStr;
+                        pointStr = ConvertDecimals(points);
+                    }
+                }
+                val = String.Format("{0} {1}{2} {3}", ConvertWholeNumber(wholeNo).Trim(), andStr, pointStr, endStr);
+            }
+            catch { }
+            return val;
+        }
+
+        private static String ConvertDecimals(String number)
+        {
+            String cd = "", digit = "", engOne = "";
+            for (int i = 0; i < number.Length; i++)
+            {
+                digit = number[i].ToString();
+                if (digit.Equals("0"))
+                {
+                    engOne = "Zero";
+                }
+                else
+                {
+                    engOne = Ones(digit);
+                }
+                cd += " " + engOne;
+            }
+            return cd;
+        }
+
 
 
         static void Main(string[] args)
         {
+
+            string isNegative = "";
+            try
+            {
+                Console.WriteLine("Enter a Number to convert to currency");
+                string number = Console.ReadLine();
+                number = Convert.ToDouble(number).ToString();
+
+                if (number.Contains("-"))
+                {
+                    isNegative = "Minus ";
+                    number = number.Substring(1, number.Length - 1);
+                }
+                if (number == "0")
+                {
+                    Console.WriteLine("The number in currency fomat is \nZero Only");
+                }
+                else
+                {
+                    Console.WriteLine("The number in currency fomat is \n{0}", isNegative + ConvertToWords(number));
+                }
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
